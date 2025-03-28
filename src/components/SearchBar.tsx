cbc7b8dccfb9165274c8952ef02b93cf7747e8a5
LizethPatino <lizeth0330@outlook.com>
2025-03-27 19:36:26 -0500

    Improving styles button on searchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -15,20 +15,20 @@ export default function SearchBar() {
 
   return (
     <div className="flex flex-col items-center w-full px-4 sm:px-6 mt-10">
-      <div className="w-full max-w-6xl flex items-center bg-white p-2 rounded-md shadow-lg border border-gray-200 focus:ring-primary">
+      <div className="w-full max-w-6xl flex items-center bg-white p-1 sm:p-2 rounded-md shadow-lg border border-gray-200 focus:ring-primary">
         <input
           type="text"
-          className="flex-grow p-2 text-xl border-none outline-none bg-transparent w-full sm:w-auto"
+          className="flex-grow p-2 text-lg sm:text-xl border-none outline-none bg-transparent w-full sm:w-auto"
           placeholder="Type a keyword"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           onKeyDown={(e) => e.key === "Enter" && handleSearch()}
         />
         <button
-          className="bg-primary text-white px-5 py-2 rounded-md hover:bg-primary-dark transition text-sm sm:text-base flex items-center gap-2"
+          className="bg-primary text-white px-3 py-2 sm:px-5 rounded-md hover:bg-primary-dark transition text-sm sm:text-base flex items-center gap-2"
           onClick={handleSearch}
         >
-          <MagnifyingGlassIcon className="h-6 w-6" /> 
+          <MagnifyingGlassIcon className="h-5 w-5 sm:h-6 sm:w-6" /> 
           Search
         </button>
       </div>
