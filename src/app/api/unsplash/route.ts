4cfd574651cd0260ab247fba7ea5dc41cc3a8896
LizethPatino <lizeth0330@outlook.com>
2025-03-29 12:44:18 -0500

    Refactoring route unsplash api

--- src/app/api/unsplash/route.ts
+++ src/app/api/unsplash/route.ts
@@ -1,22 +1,27 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const query = searchParams.get("query") || "perros";
-  const perPage = searchParams.get("per_page") || 15;
-  const page = searchParams.get("page") || 1;
-
   try {
-    const res = await fetch(
-      `${process.env.UNSPLASH_API_URL}/search/photos?query=${query}&page=${page}&per_page=${perPage}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
-    );
+    const { searchParams } = new URL(req.url);
+    const query = searchParams.get("query") || "perros";
+    const perPage = Number(searchParams.get("per_page")) || 15;
+    const page = Number(searchParams.get("page")) || 1;
 
-    if (!res.ok) throw new Error("Failed to fetch images");
+    const apiUrl = `${process.env.UNSPLASH_API_URL}/search/photos?query=${query}&page=${page}&per_page=${perPage}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`;
+
+    const res = await fetch(apiUrl, { cache: "no-store" });
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: `Error ${res.status}: ${res.statusText}` },
+        { status: res.status }
+      );
+    }
 
     const data = await res.json();
     return NextResponse.json(data);
   } catch (error) {
     console.error("Error fetching images:", error);
-    return NextResponse.json({ error: "Failed to fetch images" }, { status: 500 });
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
