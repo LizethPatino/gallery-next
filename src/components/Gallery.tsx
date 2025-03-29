38d70614e9a3f14b8b85f41d2fbd60818a09a001
LizethPatino <lizeth0330@outlook.com>
2025-03-29 12:35:15 -0500

    Refactoring gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -16,9 +16,7 @@ export default function Gallery() {
   return (
     <div className="mx-auto px-2 max-w-full">
     {images.length > 0 ? (
-      <>
         <GalleryGrid images={images}/>
-      </>
     ) : (
       <p className="flex justify-center items-center h-[50vh] text-gray-500 text-2xl">
         No results found. Try a different search!
