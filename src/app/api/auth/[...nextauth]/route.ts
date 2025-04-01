bd155d7d62d8afbfe160070f1baf599dc5934202
LizethPatino <lizeth0330@outlook.com>
2025-03-31 22:45:31 -0500

    integration prisma-next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,26 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
 
+// Exporta la configuración de NextAuth para ser usada por Next.js
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      console.log("User ID in session:", user.id);
+      session.user.id = user.id;  // Agregar el ID del usuario a la sesión
+      return session;
+    },
+  },
+  debug: true, 
 });
 
+// Exporta la función `GET` y `POST` que Next.js usará para manejar las rutas API
 export { handler as GET, handler as POST };
--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -3,12 +3,35 @@ import { useImageStore } from "@/store/useImageStore";
 import { HeartIcon } from "@heroicons/react/24/solid";
 import { HeartIcon as HeartOutline } from "@heroicons/react/24/outline";
 import { ImageType } from "@/types";
+import { addFavorite } from "@/lib/favoritesService";
+import { useSession } from "next-auth/react";
 
-export default function GalleryItem({ img }: { img: ImageType }) {
+export default function GalleryItem({ img }: { img: ImageType}) {
   const favorites = useImageStore((state) => state.favorites);
   const toggleFavorite = useImageStore((state) => state.toggleFavorite);
 
+  const { data: session } = useSession();
   const isFavorite = favorites.some((fav) => fav.id === img.id);
+
+
+  const handleAddFavorite = async () => {
+
+    if (!session?.user?.id) {
+      console.error("User is not authenticated");
+      return;
+    }
+
+    const { id, urls, alt_description } = img;
+
+    try {
+      const userId = session.user.id; // Aquí obtienes el userId
+      const data = await addFavorite(userId, id, urls.small, alt_description);
+      console.log("Favorite added:", data);
+    } catch (error) {
+      console.error("Error adding favorite:", error);
+    }
+  };
+
 
   return (
     <div className="relative group">
@@ -20,7 +43,7 @@ export default function GalleryItem({ img }: { img: ImageType }) {
         className="rounded-lg shadow-md hover:scale-105 transition-transform"
       />
       <button
-        onClick={() => toggleFavorite(img)}
+        onClick={handleAddFavorite}
         className="absolute top-2 right-2 text-white bg-black/50 p-2 rounded-full opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity"
       >
         {isFavorite ? (
