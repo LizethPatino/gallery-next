745bf828f955d7650b97e196a659377c669c8a82
LizethPatino <lizeth0330@outlook.com>
2025-04-01 20:26:51 -0500

    refactoring FavoritesLoader to manage service LoadFavorites

--- src/components/FavoritesLoader.tsx
+++ src/components/FavoritesLoader.tsx
@@ -2,13 +2,25 @@
 
 import { useEffect } from "react";
 import { useImageStore } from "@/store/useImageStore";
+import { useSession } from "next-auth/react";
 
 export default function FavoritesLoader() {
+  const { data: session } = useSession();
   const loadFavorites = useImageStore((state) => state.loadFavorites);
 
   useEffect(() => {
-    loadFavorites();
-  }, [loadFavorites]);
+    const fetchFavorites = async () => {
+      if (session?.user?.id) {
+        try {
+          await loadFavorites(session.user.id);
+        } catch (error) {
+          console.error("Error loading favorites:", error);
+        }
+      }
+    };
+
+    fetchFavorites();
+  }, [session, loadFavorites]);
 
   return null;
 }
