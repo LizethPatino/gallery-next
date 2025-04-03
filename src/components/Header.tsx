849a743887f4b7d1de3603ba6e679c840d13261b
LizethPatino <lizeth0330@outlook.com>
2025-04-03 13:44:27 -0500

    add validation to show the favorites section if you are logged

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function Header() {
+
+  const { data: session } = useSession();
+
   return (
     <header className="h-16 bg-white shadow-md flex justify-between items-center px-6 md:px-10">
       <Link href="/" className="flex items-center">
@@ -25,11 +31,14 @@ export default function Header() {
               Inicio
             </Link>
           </li>
+          { session && 
          <li>
+  
          <Link href="/favorites" className="text-gray-700 hover:text-gray-900">
-              Favorites ❤️
-            </Link>
+              Favorites
+          </Link>
           </li> 
+}
         </ul>
       </nav>
     </header>
